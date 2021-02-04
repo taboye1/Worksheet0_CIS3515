ac96e14ee7d84c87ea54d40171adf9e35945129e
taboye1 <58162039+taboye1@users.noreply.github.com>
2021-02-03 20:46:23 -0500

    Add files via upload
    I created a class called Shape Using NetBeans

---
+++ Shape.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author taccp
+ */
+public class Shape {
+    
+    private String name;
+    
+    public Shape(String name){
+        this.name = name;
+    }
+    
+    /**returns the name of the shape*/
+    public String getName(){
+        return name;
+    }
+    
+    /**returns the area of the shape*/
+    public double getArea(){
+        return 0.0;
+    }
+   /** print the dimensions of the shape  */
+    public void printDimensions(){
+        System.out.println("No dimensions");
+    }
+    
+}
